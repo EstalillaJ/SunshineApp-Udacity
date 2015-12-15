f61860f19de421c6fc6e55e2c34c7b9e3a7fb024
Estalillaj <joshtestalilla@gmail.com>
2015-12-15 09:54:11 -0800

    Changed toast to an explicit intent to start DetailActivity

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,9 +86,9 @@ public class ForecastFragment extends Fragment {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                     String forecast = mForecastAdapter.getItem(i);
-                    Toast toast = Toast.makeText(getActivity(),forecast,Toast.LENGTH_SHORT);
-                    toast.show();
-
+                    Intent intent = new Intent(getActivity(), DetailActivity.class);
+                    intent.putExtra("forecast",forecast);
+                    startActivity(intent);
                 }
             });
             return rootView;
